db95e11ffa8aab2a3d6b1538384281c62b5386f4
Martin Lenart <martin_lenart@icloud.com>
2022-01-25 11:23:26 +0100

    Update Trivial and NonTrivial Binding

---
+++ DataBindingExamples/DataBindingExamples/Views/NonTrivialBinding1.xaml.cs
@@ -0,0 +1,86 @@
+﻿using DataBindingExamples.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace DataBindingExamples.Views
+{
+    public partial class NonTrivialBinding1 : ContentPage
+    {
+        ItemA item;
+        Random rnd;
+
+        public NonTrivialBinding1()
+        {
+            InitializeComponent();
+            item = new ItemA { Message = "A message from item", Creation = DateTime.Now};
+            rnd = new Random();
+
+            this.BindingContext = item;
+        }
+
+        public async void ButtonClicked(object sender, EventArgs args)
+        {
+            await DisplayAlert("Values of Item", $"Message: {item.Message}\nCreation: {item.Creation:F}", "OK");
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            item.Message = $"A message set in C# code with a random number: {rnd.Next(1000)}";
+            item.Creation = item.Creation + new TimeSpan(10, 10, 10); // add 10 days, 10 hours, 10 minutes 
+        }
+    }
+
+    public class ItemA
+    {
+        public string Message { get; set; }
+        public DateTime Creation { get; set; }
+    }
+
+    #region INotifyPropertyChange
+    public class ItemB : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string _message;
+
+        public string Message
+        {
+            set
+            {
+                if (value != _message)
+                {
+                    _message = value;
+                    OnPropertyChanged("Message");
+                }
+            }
+            get => _message;
+        }
+
+        DateTime _creation;
+
+        public DateTime Creation
+        {
+            set
+            {
+                if (value != _creation)
+                {
+                    _creation = value;
+                    OnPropertyChanged("Creation");
+                }
+            }
+            get => _creation;
+        }
+
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    #endregion
+
+
+}
+
---
+++ DataBindingExamples/DataBindingExamples/Views/TrivialBinding.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DataBindingExamples.ViewModels;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace DataBindingExamples.Views
+{
+    public partial class TrivialBinding : ContentPage
+    {
+        public TrivialBinding()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
